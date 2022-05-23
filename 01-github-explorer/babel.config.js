e489658c5ab8ade7a4196be892736bcab33d6c3d
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-22 22:32:33 -0400

    COnfiguração do typescript

--- 01-github-explorer/babel.config.js
+++ 01-github-explorer/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    presets:[
+    presets: [
         '@babel/preset-env',
+        '@babel/preset-typescript',
         ['@babel/preset-react', {
             runtime: 'automatic'
         }]
