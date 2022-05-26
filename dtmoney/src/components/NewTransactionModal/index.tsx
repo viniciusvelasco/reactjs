ca17d853beab11739ff1b28660a2fbe34560cd5c
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-25 23:17:37 -0400

    Botão de tipo de lançamento

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Modal from "react-modal";
-import CloseImg from '../../assets/close.svg';
-import { Container } from "./styles";
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, RadioBox, TransctionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState<'deposit' | 'withdraw'>('deposit');
+
     return (
         <Modal
             isOpen={isOpen}
@@ -18,12 +23,34 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 type="button"
                 onClick={onRequestClose}
                 className="react-modal-close">
-                <img src={CloseImg} alt="Fechar" />
+                <img src={closeImg} alt="Fechar" />
             </button>
             <Container>
                 <h2>Cadastrar Transação</h2>
                 <input type="text" placeholder="Titulo" />
                 <input type="number" placeholder="Valor" />
+
+                <TransctionTypeContainer>
+                    <RadioBox
+                        type="button"
+                        isActive={type === 'deposit'}
+                        activeColor="green"
+                        onClick={() => { setType('deposit') }}
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+                    <RadioBox
+                        type="button"
+                        isActive={type === 'withdraw'}
+                        activeColor="red"
+                        onClick={() => { setType('withdraw') }}
+                    >
+                        <img src={outcomeImg} alt="Entrada" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransctionTypeContainer>
+
                 <input type="text" placeholder="Categoria" />
 
                 <button type="submit">
--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,4 @@
+import { darken, transparentize } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.form`
@@ -28,7 +29,7 @@ export const Container = styled.form`
     }
   }
 
-    button {
+    button[type="submit"] {
         width: 100%;
         padding: 0 1.5rem;
         height: 4rem;
@@ -47,3 +48,52 @@ export const Container = styled.form`
         }
     }
 `
+
+export const TransctionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33cc95',
+  red: '#e52e4d',
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7d7;
+    border-radius: 0.25rem;
+
+    background-color: ${(props) => props.isActive 
+      ? transparentize(0.9, colors[props.activeColor])
+      : 'transparent'};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+      border-color: ${darken(0.1, '#d7d7d7d7')};
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
+    }  
+`;
