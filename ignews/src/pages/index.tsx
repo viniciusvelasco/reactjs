54e002a67d36bd5f3eb2f7b0c0baeebb812ce38d
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-28 15:47:49 -0400

    Alterar para static

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -33,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getStaticProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1L4SuJBSrBynysejpppd6aRW');
 
   const product = {
@@ -45,5 +45,6 @@ export const getStaticProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
