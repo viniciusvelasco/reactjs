815355f6fa633f634b385c1aa243688a54e02c28
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-28 01:15:30 -0400

    Configuração do sass

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,5 +1,6 @@
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
