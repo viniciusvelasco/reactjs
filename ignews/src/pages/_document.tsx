5edfc0cac3e369d8b3744c9ff8485227ed7fb66c
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-28 01:39:26 -0400

    Configuração do css base

--- ignews/src/pages/_document.tsx
+++ ignews/src/pages/_document.tsx
@@ -10,7 +10,6 @@ export default class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
                 </Head>
 
-                <title>ig.news</title>
                 <body>
                     <Main />
                     <NextScript />
