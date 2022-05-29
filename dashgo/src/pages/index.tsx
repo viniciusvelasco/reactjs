4768097386b73006999c8829680332463e0d9763
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-29 19:35:08 -0400

    Criação do login, dashboard e sidebar

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,6 +1,30 @@
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="Email" type="email" />
+          <Input name="senha" label="Senha" type="password" />
+        </Stack>
+
+        <Button type="submit" mt="6" size="lg" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
