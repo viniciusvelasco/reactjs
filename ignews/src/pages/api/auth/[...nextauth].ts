fbea6ad3bebd0aa92e23706ee0a063d7aee04ea9
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-28 22:43:15 -0400

    Pagamento via stipe

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -13,4 +15,40 @@ export default NextAuth({
             }
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile, credentials }): Promise<boolean> {
+            try {
+                const {email} = user;
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            {
+                                data: {
+                                    email,
+                                }
+                            }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                            )
+                        )
+                    )
+                );
+                return true;
+            } catch {
+                return false; 
+            }
+        }
+    }
 });
