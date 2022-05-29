4286bd47866f330a146f755bde6f7ff60d0adb07
Vinícus Velasco <vinicius.velasco@live.com>
2022-05-29 17:54:47 -0400

    Configuração do Prismic

---
+++ ignews/.slicemachine/assets/slices/Post/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Post';
+
+export default {
+  title: 'slices/Post'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"post","items":[],"primary":{"title":[{"type":"heading1","text":"Engage web-enabled mindshare","spans":[]}],"description":[{"type":"paragraph","text":"Anim consectetur proident esse esse esse sint culpa labore deserunt amet cupidatat. Qui enim ad officia.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
